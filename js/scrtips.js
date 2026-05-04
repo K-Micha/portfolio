565ae82e9a19d684b3b31cd1b81a10d6d8b8c147
K-Micha <Micha.kuehn91@gmail.com>
2026-05-04 16:31:38 +0200

    fixed basic page

--- js/scrtips.js
+++ js/scrtips.js
@@ -1,3 +1,5 @@
+let currentLang = "en";
+
 function scrollToSkills() {
     let target = document.getElementById("skills");
     if (!target) return;
@@ -22,3 +24,42 @@ function scrollToContact() {
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: "smooth" });
 }
+
+function toggleLangMenu() {
+    document.querySelector(".lang").classList.toggle("open");
+}
+
+function setLang(event, lang) {
+    event.stopPropagation();
+
+    currentLang = lang;
+    localStorage.setItem("lang", lang);
+
+    updateLangSwitch();
+    document.querySelector(".lang").classList.remove("open");
+}
+
+function updateLangSwitch() {
+    let active = document.getElementById("activeLang");
+    let options = document.getElementById("langOptions");
+
+    active.textContent = currentLang.toUpperCase();
+
+    let other = currentLang === "en" ? "de" : "en";
+
+    options.innerHTML =
+        `<button onclick="setLang(event, '${other}')">
+            ${other.toUpperCase()}
+        </button>`;
+
+    active.classList.add("active");
+}
+
+function loadLang() {
+    let saved = localStorage.getItem("lang");
+    if (saved) currentLang = saved;
+
+    updateLangSwitch();
+}
+
+loadLang();
